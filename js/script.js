be938915ec3c9e1c0abd9c11253d09a877dc96fb
Joseph Flores <yosept.flores@gmail.com>
2020-04-07 15:16:56 -0500

    We made the basic html layout, with a form and a place to interact from the js

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let myLibrary = [];
+
+function Book() {
+  // the constructor...
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+htmlBook = `
+  <h2 id="bookTitle"></h2>
+  <div id="pages"></div>
+  <div id="status"></div>
+`;
