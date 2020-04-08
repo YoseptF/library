d76ec33aff502f6a539590b44562ad0e0bb087ea
Yair Fernando Facio Fraga <yair.facio11@gmail.com>
2020-04-07 19:13:14 -0500

    Fixed linters and deleted node_modules from git

--- js/script.js
+++ js/script.js
@@ -17,69 +17,60 @@ function addBookToLibrary(book) {
 }
 
 function addBooksToDOM(book) {
-  let htmlBook = `
+  const htmlBook = `
   <div class="card-body card mb-3 col-4 mx-auto text-center" data-id="${
-    book.id
-  }">
+  book.id
+}">
     <h2 id="bookTitle">${book.title}</h2>
     <div id="pages">${book.author}</div>
     <div id="pages">${book.pages}</div>
-    <div id="status">${book.status ? "read" : "un-read"}</div>
+    <div id="status">${book.status ? 'read' : 'un-read'}</div>
     <button class="btn btn-primary btn-block">Read</button>
     <button class="btn btn-danger btn-block">Delete</button>
   </div>
 `;
 
-  document.getElementById("books").innerHTML += htmlBook;
+  document.getElementById('books').innerHTML += htmlBook;
 }
 
-window.onload = (event) => {
-  
+window.onload = () => {
   if (localStorage.getItem('books') === null) {
     localStorage.setItem('books', JSON.stringify(myLibrary));
   } else {
-    myLibrary = JSON.parse(localStorage.getItem('books'))
-    for (book of myLibrary) {
+    myLibrary = JSON.parse(localStorage.getItem('books'));
+    for (const book of myLibrary) {
       addBooksToDOM(book, myLibrary.indexOf(book));
     }
   }
 };
 
-document.querySelector("#searchBook").addEventListener("submit", submitForm);
-document
-  .querySelector("#books")
-  .addEventListener("click", (e) => deleteBook(e.target));
-document
-  .querySelector("#books")
-  .addEventListener("click", (e) => toogleBook(e.target));
 
 function submitForm(e) {
   e.preventDefault();
-  let title = document.querySelector("#title").value;
-  let author = document.querySelector("#author").value;
-  let pages = document.querySelector("#pages").value;
-  let status = document.querySelector("#status").checked;
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const status = document.querySelector('#status').checked;
 
   if (!title || !author || !pages) {
-    alert("Please fill in the form");
+    alert('Please fill in the form');
     return false;
   }
 
-  let book = new Book(title, author, pages, status);
+  const book = new Book(title, author, pages, status);
   addBookToLibrary(book);
   addBooksToDOM(book);
-  console.log(myLibrary);
   localStorage.setItem('books', JSON.stringify(myLibrary));
-  document.getElementById("searchBook").reset();
+  document.getElementById('searchBook').reset();
 }
 
 function deleteBook(e) {
-  if (e.classList.contains("btn-danger")) {
-    let book = e.parentElement;
-    let id = book.getAttribute("data-id");
+  if (e.classList.contains('btn-danger')) {
+    const book = e.parentElement;
+    const id = book.getAttribute('data-id');
     book.remove();
     for (let i = 0; i < myLibrary.length; i++) {
-      if ((i = id)) {
+      if (i === id) {
         myLibrary.splice(i, 1);
       }
     }
@@ -88,17 +79,19 @@ function deleteBook(e) {
 }
 
 function toogleBook(e) {
-  if (e.classList.contains("btn-primary")) {
-    let book = e.parentElement;
-    let id = book.getAttribute("data-id");
-    let actualStatus = document.querySelector(`div[data-id='${id}'] > #status`)
+  if (e.classList.contains('btn-primary')) {
+    const book = e.parentElement;
+    const id = book.getAttribute('data-id');
+    const actualStatus = document.querySelector(`div[data-id='${id}'] > #status`)
       .innerHTML;
-    document.querySelector(`div[data-id='${id}'] > #status`).innerHTML =
-      actualStatus === "read" ? "un-read" : "read";
-    for (elem of myLibrary) {
-      if (elem.id == id) elem.toggleRead();
+    document.querySelector(`div[data-id='${id}'] > #status`).innerHTML = actualStatus === 'read' ? 'un-read' : 'read';
+    for (const elem of myLibrary) {
+      if (elem.id === id) elem.toggleRead();
     }
     localStorage.setItem('books', JSON.stringify(myLibrary));
-    console.log(myLibrary);
   }
 }
+
+document.querySelector('#searchBook').addEventListener('submit', submitForm);
+document.querySelector('#books').addEventListener('click', (e) => deleteBook(e.target));
+document.querySelector('#books').addEventListener('click', (e) => toogleBook(e.target));
